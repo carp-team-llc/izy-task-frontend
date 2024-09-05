1faa430e09cf5f1534deba6f254f6169809c2267
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-09-05 17:14:59 +0700

    initApi services and build hook for using api

---
+++ src/hook/Api/auth/useLogin.ts
@@ -0,0 +1,34 @@
+import { useMutation } from '@tanstack/react-query'
+import rootApi from '../../../services/initApi'
+import endpoint from '../../../services/endpoint'
+
+type LoginParams = {
+  email: string;
+  password: string;
+}
+
+type Response = {
+  data: any;
+}
+
+const useLogin = () => {
+  const { isError, data, error, mutateAsync } = useMutation({
+    mutationFn: (variables: LoginParams) => {
+      return rootApi.post<LoginParams, Response>(
+        endpoint.login,
+        variables
+      )
+    },
+    onError: (e: any) => {
+      alert(e?.response?.data?.message || 'Đã có lỗi xảy ra')
+    }
+  })
+  return {
+    isError,
+    data,
+    error,
+    onLogin: mutateAsync
+  }
+}
+
+export default useLogin
